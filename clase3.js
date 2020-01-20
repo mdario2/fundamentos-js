d4cb154b33f1a0c746cffb542f2079888be19eca
Manuel <mdariogallardo@gmail.com>
2020-01-19 22:19:24 -0500

    clase 3: operaciones con numeros en js

---
+++ clase3.js
@@ -0,0 +1,25 @@
+var edad = 24
+edad+=1
+
+var peso =75
+peso-=2
+
+var sandwich = 1
+
+peso += sandwich
+
+var jugarFutbol = 3
+
+peso -=jugarFutbol
+
+var precioVino = 200.3
+
+var total = Math.round(precioVino * 100 * 3) / 100	
+var totalStr = total.toFixed(2)
+var total2 = parseFloat(totalStr)
+
+//division
+
+var pizza = 8,personas=2
+
+var reparto = pizza / personas
