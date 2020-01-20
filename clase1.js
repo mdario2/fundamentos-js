60b69785df4f5a7ff4a4c8ff13b25a36d8d613ea
Manuel <mdariogallardo@gmail.com>
2020-01-19 20:54:23 -0500

    trbajando con string en js

---
+++ clase1.js
@@ -0,0 +1,15 @@
+var nombre = 'Manuel Dario', apellido = 'Gallardo'
+
+//convertir a mayuscula
+
+var nombreEnMayus = nombre.toUpperCase()
+//convertir a minuscula
+var apellidoEnMinus = apellido.toLowerCase()
+
+var primeraLetra =nombre.charAt(0)
+
+var cantletras = nombre.length
+
+var fullName = `${nombre} ${apellido.toUpperCase()}`
+//subcadena
+var str = nombre.substr(1,4)
