224b7cd55daf7b13b3de773f5023cb16a2bc0ab4
Manuel <mdariogallardo@gmail.com>
2020-01-30 00:10:25 -0500

    arrow functions

--- clase10.js
+++ clase10.js
@@ -1,12 +1,20 @@
 var boris = {
 	nombre: 'Boris',
 	apellido: 'Gomez',
-	edad: 33,
+	edad: 13,
 	ingeniero: false,
 	cocinero: false,
 	cantante: true,
 	fotografo: false
 }
+
+var fabio = {
+	nombre:'fabio',
+	apellido:'bermudez',
+	edad: 28
+}
+
+
 //condicionales
 
 function printProfession(persona)
@@ -36,4 +44,29 @@ function printProfession(persona)
 
 }
 
-printProfession(boris)
+const MAYORIA_DE_EDAD = 18
+
+/*const esMayordeEdad = function (persona) {
+	return persona.edad >= MAYORIA_DE_EDAD
+}*/
+
+//usando arrow function lo anterior quedaria
+const esMayordeEdad = persona => persona.edad >= MAYORIA_DE_EDAD
+
+
+
+function printSiesMayorDeEdad(persona)
+{
+	if (esMayordeEdad(persona)) 
+	{
+		console.log(`${persona.nombre} es mayor de edad`)
+	}else
+	{
+		console.log(`${persona.nombre} es menor de edad`)
+	}
+}
+
+
+ 
+//printProfession(boris)
+
