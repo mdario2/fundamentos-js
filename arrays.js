f77e7f631a1f50d1df779f0b435d907d3c2a9751
Manuel <mdariogallardo@gmail.com>
2020-02-04 00:14:35 -0500

    probando metodo reduce

--- arrays.js
+++ arrays.js
@@ -1,31 +1,36 @@
 var paula ={
 	nombre:'Paula',
 	apellido:'Barrios',
-	altura:1.76
+	altura:1.76,
+	cantidadLibros: 52
 }
 
 var vicky ={
 	nombre:'Vicky',
 	apellido:'Zapata',
-	altura:1.56
+	altura:1.56,
+	cantidadLibros: 111
 }
 
 var sascha ={
 	nombre:'Sascha',
 	apellido:'Fitness',
-	altura:1.72
+	altura:1.72,
+	cantidadLibros: 10
 }
 
 var martin ={
 	nombre:'Martin',
 	apellido:'Diaz',
-	altura:1.85
+	altura:1.85,
+	cantidadLibros: 81
 }
 
 var dario ={
 	nombre:'Dario',
 	apellido:'Gomez',
-	altura:1.71
+	altura:1.71,
+	cantidadLibros: 63
 }
 
 const esAlta = persona => persona.altura > 1.8
@@ -46,7 +51,12 @@ const pasarAlturaACms = persona => ({
 	})
 
 
-
-
 var personasCms = personas.map(pasarAlturaACms)
-console.log(personasCms)
+//reduce---------------------------------------
+const reducer = (acum, {cantidadLibros}) =>  acum + cantidadLibros
+
+
+var totalDeLibros = personas.reduce(reducer,0)
+
+
+console.log(`En total todos tienen ${totalDeLibros} libros`)
