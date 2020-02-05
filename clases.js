c98d48a96d422860ca8a85c2abf7f2b82f97db32
Manuel <mdariogallardo@gmail.com>
2020-02-04 23:44:09 -0500

    modificando un prototipo

--- clases.js
+++ clases.js
@@ -1,15 +1,22 @@
-function Persona(nombre, apellido){
+function Persona(nombre, apellido,altura){
 	this.nombre = nombre
 	this.apellido = apellido
+	this.altura = altura
 }
 
 
-Persona.prototype.saludar = function ()
+Persona.prototype.saludar = () =>
 {
 	console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
 }
 
+Persona.prototype.soyAlto = () => this.altura > 1.8
 
-var manuel = new Persona('Manuel','Gallardo')
-var erika = new Persona('Erika','Fontalvo')
-var arturo= new Persona('Arturo','Vidal')
+
+var manuel = new Persona('Manuel','Gallardo',1.75)
+var erika = new Persona('Erika','Fontalvo',1.65)
+var arturo= new Persona('Arturo','Vidal',1.89)
+
+manuel.soyAlto()
+erika.soyAlto()
+arturo.soyAlto()
