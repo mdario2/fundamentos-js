0c34e3146638460784a97c5d7d45793f43175d9a
Manuel <mdariogallardo@gmail.com>
2020-02-05 22:49:25 -0500

    bug arrow function con el this

--- clases.js
+++ clases.js
@@ -5,17 +5,19 @@ function Persona(nombre, apellido,altura){
 }
 
 
-Persona.prototype.saludar = () =>
+Persona.prototype.saludar = function()
 {
 	console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
 }
 
-Persona.prototype.soyAlto = () => this.altura > 1.8
+Persona.prototype.soyAlto = function(){
+	return this.altura > 1.8	
+} 
 
 
 var manuel = new Persona('Manuel','Gallardo',1.75)
 var erika = new Persona('Erika','Fontalvo',1.65)
-var arturo= new Persona('Arturo','Vidal',1.89)
+var arturo= new Persona('Arturo','Vidal', 1.89)
 
 manuel.soyAlto()
 erika.soyAlto()
