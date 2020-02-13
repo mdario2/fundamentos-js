ed812cdc47f470c6d52c2d17d7bdd23aced70d39
Manuel <mdariogallardo@gmail.com>
2020-02-13 15:22:22 -0500

    haciendo multiples request

--- callbacks.js
+++ callbacks.js
@@ -1,13 +1,20 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
-//request con jQuery
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = { crossDomain: true}
 
 const onResponse = function(luke){
 	console.log(`Hola yo soy ${luke.name} y mi altura es: ${luke.height}`)
 }
 
-$.get( lukeUrl , opts , onResponse)
+
+function obtenerPersonaje(id)
+{
+	const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+	//request
+	$.get( url , opts , onResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
