b8a152125c8ae3f3bf363644ed6c677f6144f6c8
Manuel <mdariogallardo@gmail.com>
2020-02-13 16:14:57 -0500

    manejando el orden y el asincronismo en js

--- callbacks.js
+++ callbacks.js
@@ -3,18 +3,29 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = { crossDomain: true}
 
-const onResponse = function(luke){
-	console.log(`Hola yo soy ${luke.name} y mi altura es: ${luke.height}`)
-}
 
 
-function obtenerPersonaje(id)
+function obtenerPersonaje(id,callback)
 {
 	const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
 	//request
-	$.get( url , opts , onResponse)
+	$.get( url , opts , function(luke){
+	console.log(`Hola yo soy ${luke.name} y mi altura es: ${luke.height}`)
+
+	if(callback)
+	{
+		callback()
+	}
+})
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, function(){
+	obtenerPersonaje(2, function () {
+		obtenerPersonaje(3, function () {
+			obtenerPersonaje(4 , function () {
+				obtenerPersonaje(5)
+			})
+		})
+	})
+})
+
